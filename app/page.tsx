aa118ef04e7ef48875c21e1a682c0dd4f64aa321
Anjali Daharwal <anjalidaharwal5@gmail.com>
2025-02-27 22:03:02 +0530

    Initial setup: Added project files and configurations

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Let's go ahead!</h1>
+      <Button>Click me</Button>
+    </main>
+   
+
+
+  );
+}
