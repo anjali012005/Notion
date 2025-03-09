26ba81f04ea61c5f1f4322fa9f2340053e9d5951
Anjali Daharwal <anjalidaharwal5@gmail.com>
2025-03-09 12:21:03 +0530

    Implemented liveblocks for real-time collaboration

---
+++ app/doc/[id]/layout.tsx
@@ -0,0 +1,29 @@
+import { auth, currentUser } from '@clerk/nextjs/server';
+import React from 'react'
+
+const DocLayout =()=> async({children, params: {id}} : {children:React.ReactNode, params: {id:string}}) =>{
+    const user = await currentUser(); // Get user details properly
+
+    console.log("User Data:", user);
+    const { sessionClaims, userId } = await auth();
+
+    if (!userId) {
+        throw new Error("Unauthorized: No user found");
+    }
+
+    const userEmail = sessionClaims?.email;
+
+    if (!userEmail) {
+        throw new Error("Error: User email is undefined");
+    }
+
+    console.log("Session Email:", sessionClaims?.email);
+    return (
+        <div>
+            {children}
+        </div>
+    )
+}
+
+export default DocLayout
+
---
+++ components/RoomProvider.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const RoomProvider = () => {
+    return (
+        <div>
+            RoomProvider
+        </div>
+    )
+}
+
+export default RoomProvider
