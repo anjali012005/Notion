7b50997b3ecce9d9feb65a2af66eea20a0a48605
Anjali Daharwal <anjalidaharwal5@gmail.com>
2025-03-11 18:55:57 +0530

    Implemented User Management Features

--- components/Document.tsx
+++ components/Document.tsx
@@ -10,6 +10,7 @@ import Editor from './Editor';
 import useOwner from '@/lib/useOwner';
 import DeleteDocument from './DeleteDocument';
 import InviteUser from './InviteUser';
+import ManageUsers from './ManageUsers';
 
 
 
@@ -64,7 +65,8 @@ const Document = ({ id }: { id: string }) => {
                 </form>
             </div>
 
-            <div>
+            <div className='flex max-w-6xl mx-auto justify-between items-center mb-5'>
+                <ManageUsers/>
                 {/* ManageUsers */}
 
                 {/* Avatars */}
--- components/actions/actions.ts
+++ components/actions/actions.ts
@@ -125,3 +125,33 @@ export async function inviteUserToDocument(roomId: string, email: string) {
         return { success: false };
     }
 }
+
+export async function removeUserFromDocument(roomId: string, email: string) {
+    const user = await currentUser(); // Get user details properly
+
+    const { sessionClaims, userId } = await auth();
+
+    if (!userId) {
+        throw new Error("Unauthorized: No user found");
+    }
+
+    const userEmail = sessionClaims?.email;
+
+    if (!userEmail) {
+        throw new Error("Error: User email is undefined");
+    }
+
+    try {
+        await adminDb
+            .collection("users")
+            .doc(email)
+            .collection("rooms")
+            .doc(roomId)
+            .delete();
+
+        return { success: true };
+    } catch (error) {
+        console.error(error);
+        return { success: false };
+    }
+}
