126f8c9e3db4a986a299e59a0922a9ca744bc03e
Anjali Daharwal <anjalidaharwal5@gmail.com>
2025-03-12 18:34:53 +0530

    Implemented User Avatars

--- components/Document.tsx
+++ components/Document.tsx
@@ -11,6 +11,7 @@ import useOwner from '@/lib/useOwner';
 import DeleteDocument from './DeleteDocument';
 import InviteUser from './InviteUser';
 import ManageUsers from './ManageUsers';
+import Avatars from './Avatars';
 
 
 
@@ -67,9 +68,8 @@ const Document = ({ id }: { id: string }) => {
 
             <div className='flex max-w-6xl mx-auto justify-between items-center mb-5'>
                 <ManageUsers/>
-                {/* ManageUsers */}
 
-                {/* Avatars */}
+                <Avatars />
             </div>
 
             <hr className='pb-10' />
