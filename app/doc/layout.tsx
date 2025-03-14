b90a262275c1e50d039f5885378301447cd451a2
Anjali Daharwal <anjalidaharwal5@gmail.com>
2025-03-14 22:09:37 +0530

    final changes

--- app/doc/layout.tsx
+++ app/doc/layout.tsx
@@ -1,14 +1,30 @@
-import LiveBlocksProvider from '@/components/LiveBlocksProvider';
-import React from 'react'
+// import LiveBlocksProvider from '@/components/LiveBlocksProvider';
+// import React from 'react'
 
-const PageLayout = ({children}:{
-    children: React.ReactNode
-}) => {
+// const PageLayout = ({children}:{
+//     children: React.ReactNode
+// }) => {
+//     return (
+//         <LiveBlocksProvider>
+//             {children}
+//         </LiveBlocksProvider>
+//     )
+// }
+
+// export default PageLayout;
+
+export default function Layout({
+    children,
+    params,
+  }: {
+    children: React.ReactNode;
+    params: { id: string };
+  }) {
     return (
-        <LiveBlocksProvider>
-            {children}
-        </LiveBlocksProvider>
-    )
-}
-
-export default PageLayout;
+      <div>
+        <h1>Document ID: {params.id}</h1>
+        {children}
+      </div>
+    );
+  }
+  
