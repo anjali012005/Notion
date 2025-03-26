ef510f82fa9db02a3b197211dc4d40f7438a387f
Anjali Daharwal <anjalidaharwal5@gmail.com>
2025-03-26 23:48:59 +0530

    Final chagess~~

--- firebase-admin.ts
+++ firebase-admin.ts
@@ -11,20 +11,8 @@ import { getFirestore } from "firebase-admin/firestore";
 // import serviceKey  from "@/service_key.json";
 // import serviceKey from "./service_key.json";
 // JSON import issue fix
-// const serviceKey = require("./service_key.json");
+const serviceKey = require("./service_key.json");
 
-const serviceKey = {
-    type: "service_account",
-    project_id: process.env.FIREBASE_PROJECT_ID,
-    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-    private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
-    client_email: process.env.FIREBASE_CLIENT_EMAIL,
-    client_id: process.env.FIREBASE_CLIENT_ID,
-    auth_uri: process.env.FIREBASE_AUTH_URI,
-    token_uri: process.env.FIREBASE_TOKEN_URI,
-    auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT,
-    client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
-};
 
 let app: App;
 
